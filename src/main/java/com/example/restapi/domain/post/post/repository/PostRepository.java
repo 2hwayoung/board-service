9a616c49b35e204ce1b0bf729e55b0c329e2d74f
2hwayoung <hwa3270@gmail.com>
2025-02-05 12:39:28 +0900

    test: add post write test

--- src/main/java/com/example/restapi/domain/post/post/repository/PostRepository.java
+++ src/main/java/com/example/restapi/domain/post/post/repository/PostRepository.java
@@ -3,5 +3,8 @@ package com.example.restapi.domain.post.post.repository;
 import com.example.restapi.domain.post.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Optional<Post> findTopByOrderByIdDesc();
 }
