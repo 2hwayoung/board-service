484e8eb762533cf257309a606c2efb07051fdfd4
2hwayoung <hwa3270@gmail.com>
2025-02-06 15:37:27 +0900

    feat: setup spring security and config

---
+++ src/main/java/com/example/restapi/global/security/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.restapi.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests
+                                .requestMatchers("/h2-console/**")
+                                .permitAll()
+                                .requestMatchers(HttpMethod.GET, "/api/*/posts", "/api/*/posts/{id:\\d+}", "/api/*/posts/{postId:\\d+}/comments")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN
+                        )))
+                .csrf(AbstractHttpConfigurer::disable)
+        ;
+        return http.build();
+    }
+}
